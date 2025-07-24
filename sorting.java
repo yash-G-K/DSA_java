7a50e5c0a47fb3d00060f3642a5e67e3beaae63f
Yash-G-K <kalbhuteyash@gmail.com>
2025-07-24 16:50:43 +0530

    codes

--- sorting.java
+++ sorting.java
@@ -75,13 +75,26 @@
 
 //                                        inbuilt sort
 
+// import java.util.Arrays;
+// import java.util.Collections;
+// public class sorting {
+//     public static void main(String args[]) {
+//         int arr[] = {5, 4, 1, 3, 2};
+//         Arrays.sort(arr);
+//         System.out.println("Sorted array: " + Arrays.toString(arr));
+//         Arrays.sort(arr,0,3);
+//         System.out.println("Sorted array: " + Arrays.toString(arr));        
+//     }
+// }
+
 import java.util.Arrays;
+import java.util.Collections;
 public class sorting {
     public static void main(String args[]) {
-        int arr[] = {5, 4, 1, 3, 2};
-        Arrays.sort(arr);
+        Integer arr[] = {5, 4, 1, 3, 2};
+        Arrays.sort(arr,Collections.reverseOrder());
         System.out.println("Sorted array: " + Arrays.toString(arr));
-        Arrays.sort(arr,0,3);
+        Arrays.sort(arr,0,3,Collections.reverseOrder());
         System.out.println("Sorted array: " + Arrays.toString(arr));        
     }
 }
