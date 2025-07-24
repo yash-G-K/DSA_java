08ba84fd12d5afe282f153169f3978f98b56c5ee
Yash-G-K <kalbhuteyash@gmail.com>
2025-07-24 17:36:38 +0530

    codes

--- sorting.java
+++ sorting.java
@@ -87,14 +87,45 @@
 //     }
 // }
 
-import java.util.Arrays;
-import java.util.Collections;
+// import java.util.Arrays;
+// import java.util.Collections;
+// public class sorting {
+//     public static void main(String args[]) {
+//         Integer arr[] = {5, 4, 1, 3, 2};          //this integer is object type 
+//         Arrays.sort(arr,Collections.reverseOrder());
+//         System.out.println("Sorted array: " + Arrays.toString(arr));
+//         Arrays.sort(arr,0,3,Collections.reverseOrder());
+//         System.out.println("Sorted array: " + Arrays.toString(arr));        
+//     }
+// }
+
+
+//                                         counting sort
+import java.util.*;
 public class sorting {
-    public static void main(String args[]) {
-        Integer arr[] = {5, 4, 1, 3, 2};
-        Arrays.sort(arr,Collections.reverseOrder());
-        System.out.println("Sorted array: " + Arrays.toString(arr));
-        Arrays.sort(arr,0,3,Collections.reverseOrder());
-        System.out.println("Sorted array: " + Arrays.toString(arr));        
+    public static void countingsort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length ; i++){
+            largest = Math.max(largest ,arr[i]);
+        }
+        int count[]= new int[largest+1];
+        for(int i=0; i<arr.length ; i++){
+            count[arr[i]]++;
     }
+    //sorting
+    int j=0;
+    for(int i=0;i<count.length;i++){
+        while(count[i]>0){
+            arr[j] =i;
+            j++;
+            count[i]--;
+        }
+    }
+}
+
+public static void main(String[] args) {
+    int arr[] = {1,2,3,1,6,8,2,3,2,2};
+    countingsort(arr);
+    System.out.println("counting sort =" + Arrays.toString(arr));
+}
 }
