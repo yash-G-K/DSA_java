08f68a562a71603cec8e1957b91ed636852ec3a4
Yash-G-K <kalbhuteyash@gmail.com>
2025-07-24 16:08:30 +0530

    codes

--- sorting.java
+++ sorting.java
@@ -1,4 +1,4 @@
-import java.util.*;
+// import java.util.*;
 
 //                                     bubble sorting
 // public class sorting {
@@ -51,24 +51,37 @@ import java.util.*;
 //     }
 // }
 
+//                                              insertion sort
+// public class sorting {
+//     public static void insertion_sort(int arr[]) {
+//         int n = arr.length;
+//        for(int i=1;i<n;i++){
+//         int curr = arr[i];
+//         int prev = i-1;
+//         //finding out the  corret position to insert
+//         while(prev>=0 && arr[prev]>curr){
+//             arr[prev+1] = arr[prev];
+//             prev--;
+//        }
+//        arr[prev+1] = curr;
+//     }
+//     }
+//     public static void main(String args[]) {
+//         int arr[] = {5, 4, 1, 3, 2};
+//         insertion_sort(arr);
+//         System.out.println("Sorted array: " + Arrays.toString(arr));
+//     }
+// }
 
+//                                        inbuilt sort
+
+import java.util.Arrays;
 public class sorting {
-    public static void insertion_sort(int arr[]) {
-        int n = arr.length;
-       for(int i=1;i<n;i++){
-        int curr = arr[i];
-        int prev = i-1;
-        //finding out the  corret position to insert
-        while(prev>=0 && arr[prev]>curr){
-            arr[prev+1] = arr[prev];
-            prev--;
-       }
-       arr[prev+1] = curr;
-    }
-    }
     public static void main(String args[]) {
         int arr[] = {5, 4, 1, 3, 2};
-        insertion_sort(arr);
+        Arrays.sort(arr);
         System.out.println("Sorted array: " + Arrays.toString(arr));
+        Arrays.sort(arr,0,3);
+        System.out.println("Sorted array: " + Arrays.toString(arr));        
     }
 }
