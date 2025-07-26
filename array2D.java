ca29f9c5c1bf6c25302900fab7bd5c11253c14d2
Yash-G-K <kalbhuteyash@gmail.com>
2025-07-26 17:23:58 +0530

    codes

---
+++ array2D.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class array2D {
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+        int n = matrix.length, m = matrix[0].length;
+
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
