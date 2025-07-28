ba67bfd0616a747593238b05643383120945e7bb
Yash-G-K <kalbhuteyash@gmail.com>
2025-07-28 22:39:12 +0530

    codes

---
+++ string.java
@@ -0,0 +1,24 @@
+
+public class string {
+    public static void main(String[] args) {
+        // String str = "Hello, World!"; //1st method
+        // System.out.println(str);
+        // System.out.println(str.length());
+        // String str2 = new String("yash");//2nd method
+
+        // Scanner sc = new Scanner(System.in);//user taken
+        // System.out.print("Enter your name: ");
+        // String name = sc.nextLine(); //next only take word but next line take it as whole line
+        // System.out.println("Your name is " + name);
+        // System.out.println(name.length());
+
+
+        // sc.close();
+
+        //      concatination
+        String str = "yash";
+        String str2 = "kumar";
+        String str3 = str + " " + str2;
+        System.out.println(str3);
+    }
+}
