ce6b3492e5240ee955f21a4032cc825767e4b3b7
Yash-G-K <kalbhuteyash@gmail.com>
2025-07-29 17:09:45 +0530

    changes

--- bitmanipulation.java
+++ bitmanipulation.java
@@ -1,3 +1,5 @@
+//   below there ARE 6 CODES of bit manipulation like and ,or ,XOR,compliment,left shift,right shift
+
 // public  class bitmanipulation{
 //     public static void main(String[] args) {
 //         System.out.println(5 & 6);
@@ -28,8 +30,27 @@
 // }
 // }
 
+// public  class bitmanipulation{
+//     public static void main(String[] args) {
+//         System.out.println(6 >> 1);
+// }
+// }
+
+
+//                  bit number is even or odd
 public  class bitmanipulation{
+    public static void oddoreven(int n) {
+        int bitmask = 1;
+        if((n & bitmask )== 0){
+            System.out.println(n+" is even");
+        }else {
+            System.out.println(n+" is odd");
+
+        }
+    }
     public static void main(String[] args) {
-        System.out.println(6 >> 1);
+        oddoreven(3);
+        oddoreven(11);
+        oddoreven(8);
 }
 }
