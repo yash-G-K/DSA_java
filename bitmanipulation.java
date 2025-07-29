ca176217bcbfaec6ffaf5728e7cf0b79469da493
Yash-G-K <kalbhuteyash@gmail.com>
2025-07-29 16:52:20 +0530

    changes

--- bitmanipulation.java
+++ bitmanipulation.java
@@ -10,8 +10,26 @@
 // }
 // }
 
+// public  class bitmanipulation{
+//     public static void main(String[] args) {
+//         System.out.println(5 ^ 6);
+// }
+// }
+
+// public  class bitmanipulation{
+//     public static void main(String[] args) {
+//         System.out.println(~0);
+// }
+// }
+
+// public  class bitmanipulation{
+//     public static void main(String[] args) {
+//         System.out.println(5 << 2);
+// }
+// }
+
 public  class bitmanipulation{
     public static void main(String[] args) {
-        System.out.println(5 ^ 6);
+        System.out.println(6 >> 1);
 }
 }
