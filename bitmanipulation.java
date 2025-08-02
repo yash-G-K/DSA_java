7aa9468ff5d344729b8d2978f1afc8b56e8e2f4c
yash kalbhute <kalbhuteyash@gmail.com>
2025-08-02 22:46:41 +0530

    bitmanipulation.java

--- bitmanipulation.java
+++ bitmanipulation.java
@@ -171,5 +171,21 @@
             System.out.println(fastexpo(5,3));
         }
  }
-
+//
+ public  class bitmanipulation{
+     public static int fastexpo(int a,int n) {
+        int ans = 1;
+        while (n > 0) {
+            if ((n & 1) != 0) { //check our lsb
+            ans = ans * a;
+            }
+            a = a*a;
+            n = n >> 1;
+            }
+            return ans;
+     }
+         public static void main(String[] args) {
+            System.out.println(fastexpo(5,3));
+        }
+ }
 
