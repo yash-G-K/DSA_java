d723c89100b4b2d89d4cdd379f848635e4e5ba37
yash kalbhute <kalbhuteyash@gmail.com>
2025-08-02 22:47:51 +0530

    bitmanipulation.java

--- bitmanipulation.java
+++ bitmanipulation.java
@@ -154,7 +154,7 @@
 //  }
 
 
-//
+//.                               fast exponential 
  public  class bitmanipulation{
      public static int fastexpo(int a,int n) {
         int ans = 1;
@@ -172,20 +172,4 @@
         }
  }
 //
- public  class bitmanipulation{
-     public static int fastexpo(int a,int n) {
-        int ans = 1;
-        while (n > 0) {
-            if ((n & 1) != 0) { //check our lsb
-            ans = ans * a;
-            }
-            a = a*a;
-            n = n >> 1;
-            }
-            return ans;
-     }
-         public static void main(String[] args) {
-            System.out.println(fastexpo(5,3));
-        }
- }
-
+ 
