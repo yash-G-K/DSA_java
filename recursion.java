5b59278b342cf7fd08bd9e360b0a91d814e37494
Yash-G-K <kalbhuteyash@gmail.com>
2025-08-06 23:35:53 +0530

    code changes

--- recursion.java
+++ recursion.java
@@ -1,1 +1,16 @@
 //                      recursion
+// print no from n to 1 (decreasing order)
+
+public class recursion{
+    public static void numbers(int n) {
+        if (n == 0) {
+            return;
+    }
+    System.out.print(n +" ");
+    numbers(n-1);
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        numbers(n);
+}
+}
