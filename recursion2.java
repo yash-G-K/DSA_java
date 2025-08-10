da2c82d7ca8a5e9a61e8b26fd0895641d79b76f6
Yash-G-K <kalbhuteyash@gmail.com>
2025-08-10 18:10:26 +0530

    code changes

--- recursion2.java
+++ recursion2.java
@@ -25,32 +25,62 @@
 
 
 //         remove duplicate from the string
-public class recursion2 {
-    public static void removeduplicates(String str, int idx, StringBuilder newstr, boolean[] map) {
-        if (idx == str.length()) {
-            System.out.println(newstr);
-            return;
-        }
+// public class recursion2 {
+//     public static void removeduplicates(String str, int idx, StringBuilder newstr, boolean[] map) {
+//         if (idx == str.length()) {
+//             System.out.println(newstr);
+//             return;
+//         }
 
-        char currchar = str.charAt(idx);
+//         char currchar = str.charAt(idx);
         
-        // Check if the character is a lowercase letter before accessing the map
-        if (currchar >= 'a' && currchar <= 'z') {
-            if (map[currchar - 'a']) {
-                removeduplicates(str, idx + 1, newstr, map);
-            } else {
-                map[currchar - 'a'] = true;
-                removeduplicates(str, idx + 1, newstr.append(currchar), map);
-            }
-        } else {
-            // If not a lowercase letter, append it and continue
-            removeduplicates(str, idx + 1, newstr.append(currchar), map);
-        }
-    }
+//         // Check if the character is a lowercase letter before accessing the map
+//         if (currchar >= 'a' && currchar <= 'z') {
+//             if (map[currchar - 'a']) {
+//                 removeduplicates(str, idx + 1, newstr, map);
+//             } else {
+//                 map[currchar - 'a'] = true;
+//                 removeduplicates(str, idx + 1, newstr.append(currchar), map);
+//             }
+//         } else {
+//             // If not a lowercase letter, append it and continue
+//             removeduplicates(str, idx + 1, newstr.append(currchar), map);
+//         }
+//     }
 
+//     public static void main(String[] args) {
+//         String str = "yash kalbhute";
+//         removeduplicates(str, 0, new StringBuilder(""), new boolean[26]);
+//     }
+//}
+ 
+
+
+//              friends pairing problem
+// Given n friends, each one can remain single or can be paired up with
+//  some other friend. Each friend can be paired only once. Find out the 
+//  total number of ways in which friends can remain 
+//  single or can be paired up.
+
+public class recursion2{
+public static int friendspairing(int n){
+    if(n==1 || n==2){
+        return n;
+    }
+//    single choices
+     int  fnm1 = friendspairing(n-1);
+
+     // pairing
+     int fnm2 = friendspairing(n-2);
+     int pairways = (n-1) * (n-2);
+
+     //total pairing
+     int totalways = fnm1 + pairways;
+         return totalways;
+
+}
     public static void main(String[] args) {
-        String str = "yash kalbhute";
-        removeduplicates(str, 0, new StringBuilder(""), new boolean[26]);
+        int  n=5;
+        System.out.println(friendspairing(n));
     }
 }
- 
