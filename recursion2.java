4df092ed3d35e65a70faff0c42d937ae85eb0202
Yash-G-K <kalbhuteyash@gmail.com>
2025-08-12 21:35:10 +0530

    code changes

--- recursion2.java
+++ recursion2.java
@@ -89,6 +89,23 @@
 
 //                     Binary string problem
 // print all binary strings of size N without consecutive ones.
+// public class recursion2{
+//     public static void binarystring(int n, int lastplace,  String str){
+//         if(n==0){
+//             System.out.println(str);
+//             return;
+//             }
+//         binarystring(n-1, 0, str+"0");
+//         if(lastplace == 0){
+//         binarystring(n-1, 1, str+"1");
+//     }
+//     }
+//     public static void main(String[] args) {
+//     int n=3;
+//     binarystring(n, 0, "");
+//     }
+// }
+
 public class recursion2{
     public static void binarystring(int n, int lastplace,  String str){
         if(n==0){
