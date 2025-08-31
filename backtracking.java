dbd8f13da3afb17e59ba9fb5d66dd25457117b0b
Yash-G-K <kalbhuteyash@gmail.com>
2025-08-31 16:38:51 +0530

    code changes

--- backtracking.java
+++ backtracking.java
@@ -77,63 +77,86 @@
 
 
 //      n queens problem using backtracking
+// public class backtracking{
+//     public static boolean issafe(char board[][],int row,int col){
+//         //verticl up
+//         for(int i=row-1; i>=0; i--){
+//             if(board[i][col] == 'Q'){
+//             return false;
+//         }
+//         }
+//         //left diagonal
+//         for(int i=row-1,j=col-1; i>=0 && j>=0; i--,j--){
+//             if(board[i][j] == 'Q'){
+//                 return false;
+//             }
+//         }
+//         //right diagonal
+//         for(int i=row-1,j=col+1; i>=0 && j<board.length; i--,j++){
+//             if(board[i][j] == 'Q'){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     public static void nqueens(char board[][],int row){
+//         //base case
+//         if(row == board.length){
+//             printboard(board);
+//             count++;
+//             System.out.println("solution count: "+count);
+//             return;
+//         }
+//         //recursion
+//         for(int j=0;j<board.length;j++){
+//             if(issafe(board,row,j)){
+//                 board [row][j] = 'Q';
+//                 nqueens(board, row+1);
+//                 board[row][j] = 'x'; //backtracking step
+//             }
+//         }
+//     }
+//     public static void printboard(char board[][]){
+//         System.out.println("-----chess board-----");
+//         for(int i=0;i<board.length;i++){
+//             for(int j=0;j<board.length;j++){
+//                 System.out.print(board[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+//      static int count = 0;
+//         public static void main(String[] args) {
+//         int n = 5;
+//         char board[][] = new char[n][n];
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 board[i][j] = 'x';
+//             }
+//         }
+//         nqueens(board,0);
+//     }
+// }
+
+
+
+//      grid unique paths using backtracking
 public class backtracking{
-    public static boolean issafe(char board[][],int row,int col){
-        //verticl up
-        for(int i=row-1; i>=0; i--){
-            if(board[i][col] == 'Q'){
-            return false;
-        }
-        }
-        //left diagonal
-        for(int i=row-1,j=col-1; i>=0 && j>=0; i--,j--){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-        }
-        //right diagonal
-        for(int i=row-1,j=col+1; i>=0 && j<board.length; i--,j++){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-        }
-        return true;
-    }
-    public static void nqueens(char board[][],int row){
+    public static int gridpath(int i,int j,int n,int m){
         //base case
-        if(row == board.length){
-            printboard(board);
-            count++;
-            System.out.println("solution count: "+count);
-            return;
+        if(i==n-1 && j==m-1){
+            return 1;
+        }else if(i==n || j==m){
+            return 0;
         }
         //recursion
-        for(int j=0;j<board.length;j++){
-            if(issafe(board,row,j)){
-                board [row][j] = 'Q';
-                nqueens(board, row+1);
-                board[row][j] = 'x'; //backtracking step
-            }
-        }
+        int w1 = gridpath(i+1,j,n,m);
+        int w2 = gridpath(i,j+1,n,m);
+        return w1+w2;
     }
-    public static void printboard(char board[][]){
-        System.out.println("-----chess board-----");
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board.length;j++){
-                System.out.print(board[i][j]+" ");
-            }
-            System.out.println();
-        }
-    }
-     static int count = 0;
-        public static void main(String[] args) {
-        int n = 5;
-        char board[][] = new char[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                board[i][j] = 'x';
-            }
-        }
-        nqueens(board,0);
+    public static void main(String[] args) {
+        int n = 3;
+        int m = 3;
+        System.out.println(gridpath(0,0,n,m));
     }
 }
