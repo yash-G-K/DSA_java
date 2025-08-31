906056ca28d216888affc5d8047f2ec3314aa732
Yash-G-K <kalbhuteyash@gmail.com>
2025-08-31 17:36:10 +0530

    code changes

--- backtracking.java
+++ backtracking.java
@@ -141,22 +141,115 @@
 
 
 //      grid unique paths using backtracking
-public class backtracking{
-    public static int gridpath(int i,int j,int n,int m){
-        //base case
-        if(i==n-1 && j==m-1){
-            return 1;
-        }else if(i==n || j==m){
-            return 0;
+// public class backtracking{
+//     public static int gridpath(int i,int j,int n,int m){
+//         //base case
+//         if(i==n-1 && j==m-1){
+//             return 1;
+//         }else if(i==n || j==m){
+//             return 0;
+//         }
+//         //recursion
+//         int w1 = gridpath(i+1,j,n,m);
+//         int w2 = gridpath(i,j+1,n,m);
+//         return w1+w2;
+//     }
+//     public static void main(String[] args) {
+//         int n = 5;
+//         int m = 3;
+//         System.out.println(gridpath(0,0,n,m));
+//     }
+// }
+
+
+//      sudoko solver using backtracking
+public class backtracking {
+    public static boolean issafe(int sudoku[][], int row, int col, int digit) {
+        // column
+        for (int i = 0; i < 9; i++) {
+            if (sudoku[i][col] == digit) {
+                return false;
+            }
         }
-        //recursion
-        int w1 = gridpath(i+1,j,n,m);
-        int w2 = gridpath(i,j+1,n,m);
-        return w1+w2;
+        // row
+        for (int j = 0; j < 9; j++) {
+            if (sudoku[row][j] == digit) {
+                return false;
+            }
+        }
+        // 3*3 grid
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (sudoku[i][j] == digit) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
-    public static void main(String[] args) {
-        int n = 3;
-        int m = 3;
-        System.out.println(gridpath(0,0,n,m));
+
+    public static boolean sudokusolver(int sudoku[][], int row, int col) {
+        // base case
+        if (row == 9) {
+            return true;
+        }
+
+        // find next row and col
+        int nextrow = row;
+        int nextcol = col + 1;
+        if (col + 1 == 9) {
+            nextrow = row + 1;
+            nextcol = 0;
+        }
+
+        // skip prefilled cells
+        if (sudoku[row][col] != 0) {
+            return sudokusolver(sudoku, nextrow, nextcol);
+        }
+
+        // try filling digits
+        for (int digit = 1; digit <= 9; digit++) {
+            if (issafe(sudoku, row, col, digit)) {
+                sudoku[row][col] = digit;
+                if (sudokusolver(sudoku, nextrow, nextcol)) {
+                    return true;
+                }
+                sudoku[row][col] = 0; // backtracking step
+            }
+        }
+        return false;
+    }
+
+    public static void printsudoku(int sudoku[][]) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(sudoku[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        int sudoku[][] = {
+            {0, 0, 8, 0, 0, 0, 0, 0, 0},
+            {4, 9, 0, 1, 5, 7, 0, 0, 2},
+            {0, 0, 3, 0, 0, 4, 1, 9, 0},
+            {1, 8, 5, 0, 6, 0, 0, 2, 0},
+            {0, 0, 0, 0, 2, 0, 0, 6, 0},
+            {9, 6, 0, 4, 0, 5, 3, 0, 0},
+            {0, 3, 0, 0, 7, 2, 0, 0, 4},
+            {0, 4, 9, 0, 3, 0, 0, 5, 7},
+            {8, 2, 7, 0, 0, 9, 0, 1, 3}
+        };
+
+        if (sudokusolver(sudoku, 0, 0)) {
+            System.out.println("Solution exists:");
+            printsudoku(sudoku);
+        } else {
+            System.out.println("Solution does not exist");
+        }
     }
 }
+
