1b0459bcbac5813f67b47a39b1908a514a6b7ac4
Yash-G-K <kalbhuteyash@gmail.com>
2025-09-12 18:59:42 +0530

    code changes

--- arraylist.java
+++ arraylist.java
@@ -199,36 +199,75 @@
 // }
 
 // Container with most 
+// import java.util.ArrayList;
+// public class arraylist {
+// public static int maxArea(ArrayList<Integer> height) {
+//     int maxarea = 0;
+//     //brute force approach O(n^2)
+//     int n = height.size();
+//     for(int i = 0; i < n; i++){
+//         for(int j = i+1; j < n; j++){
+//             int ht = Math.min(height.get(i), height.get(j));
+//             int width = j - i;
+//             int area = ht * width;
+//             maxarea = Math.max(maxarea, area);
+//         }
+//     }
+//     return maxarea;
+// }
+
+// public static void main(String args[]) {
+// ArrayList<Integer> height = new ArrayList<>(); //1, 8, 6, 2, 5, 4, 8, 3, 7
+// height.add(1); 
+// height.add(8);
+// height.add(6);
+// height.add(2);
+// height.add(5);
+// height.add(4);
+// height.add(8);
+// height.add(3);
+// height.add(7);
+
+// System.out.println("The maximum area is: " + maxArea(height));
+
+// }
+// }
+
+
+// 2 pointer approach optimed approach O(n)
 import java.util.ArrayList;
 public class arraylist {
-public static int maxArea(ArrayList<Integer> height) {
-    int maxarea = 0;
-    //brute force approach O(n^2)
-    int n = height.size();
-    for(int i = 0; i < n; i++){
-        for(int j = i+1; j < n; j++){
-            int ht = Math.min(height.get(i), height.get(j));
-            int width = j - i;
-            int area = ht * width;
-            maxarea = Math.max(maxarea, area);
+        public static int maxArea(ArrayList<Integer> height) {
+            int maxarea = 0;
+            int lp=0;
+            int rp=height.size()-1;
+
+            while(lp<rp){
+                int ht = Math.min(height.get(lp), height.get(rp));
+                int width = rp-lp;
+                int currwater = ht * width;
+                maxarea = Math.max(maxarea, currwater);
+
+                if(height.get(lp) < height.get(rp)){
+                    lp++;
+                    }else{
+                        rp--;
+                    }
+                }
+            return maxarea;
+}
+        public static void main(String args[]) {
+        ArrayList<Integer> height = new ArrayList<>(); //1, 8, 6, 2, 5, 4, 8, 3, 7
+        height.add(1); 
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+        System.out.println("The maximum area is: " + maxArea(height));
+    
         }
-    }
-    return maxarea;
-}
-
-public static void main(String args[]) {
-ArrayList<Integer> height = new ArrayList<>(); //1, 8, 6, 2, 5, 4, 8, 3, 7
-height.add(1); 
-height.add(8);
-height.add(6);
-height.add(2);
-height.add(5);
-height.add(4);
-height.add(8);
-height.add(3);
-height.add(7);
-
-System.out.println("The maximum area is: " + maxArea(height));
-
-}
 }
