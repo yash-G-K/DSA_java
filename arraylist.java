48b2fec1a0ddad5b4013773550555cde8fe3ae45
Yash-G-K <kalbhuteyash@gmail.com>
2025-09-12 08:55:51 +0530

    code changes

--- arraylist.java
+++ arraylist.java
@@ -138,3 +138,33 @@
 //        System.out.println("Sorted list in descending order: " + list);
 //     }
 // }
+
+
+// multi dimensional arraylist 
+import java.util.ArrayList;
+public class arraylist {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> mainlist = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        mainlist.add(list1);
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(4);
+        list2.add(5);
+        list2.add(6);
+        mainlist.add(list2);
+
+        for(int i = 0 ; i<mainlist.size(); i++){
+            ArrayList<Integer> currlist = mainlist.get(i);
+            for(int j =0; j<currlist.size(); j++){
+                System.out.print(currlist.get(j)+" ");
+            }
+            System.out.println();
+    }
+    System.out.print("mainlist = " + mainlist);
+    }
+}
+
