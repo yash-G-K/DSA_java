dca8f30036860eb8d19e44880f8f0d02e0733554
Yash-G-K <kalbhuteyash@gmail.com>
2025-09-25 19:43:49 +0530

    code changes

---
+++ queue.java
@@ -0,0 +1,57 @@
+public class queue {
+  static class Queue{
+    static int arr[];
+    static int size;
+    static int rear;
+
+    Queue(int n){
+        arr= new int[n];
+        size = n;
+        rear =-1;
+    }
+     public static boolean isempty(){
+        return rear ==-1;
+     }
+     //add
+     public static void add(int data){
+        if(rear == size-1){
+            System.out.println("Queue is full");
+            return;
+        }
+     rear = rear+1;
+     arr[rear] = data;
+    }
+
+    // remove
+    public static int remove(){
+         if(isempty()){
+             System.out.println("empty queue");
+             return -1;
+        }
+        int front =arr[0];
+        for(int i=0; i<rear ; i++){
+        arr[i] = arr[i+1];
+    }
+    return front;
+  }
+  //peek
+  public static int peek() {
+    if(isempty()) {
+    System.out.println("empty queue");
+    return -1;
+    }
+    return arr[0];
+}
+}
+public static void main(string args[]){
+    Queue q = new Queue(5);
+    q.add(1);
+    q.add(2);
+    q.add(3);
+    q.add(4);
+    while(!q.isempty()){
+        System.out.println(q.peek());
+        q.remove();
+    }
+}
+}
