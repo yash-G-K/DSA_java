25b82a34f0fdaf176433dc24deb608174dca461d
Yash-G-K <kalbhuteyash@gmail.com>
2025-09-27 19:56:54 +0530

    code changes

--- queue.java
+++ queue.java
@@ -201,10 +201,64 @@
 
 
 // java collection framework
+// import java.util.*;
+// public class queue {
+//     public static void main(String args[]) {
+//         // Queue<Integer> q = new LinkedList<>();  //arrayDeque<>();
+//         Queue<Integer> q = new ArrayDeque<>();
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         while (!q.isEmpty()) {
+//             System.out.println(q.peek());
+//             q.remove();
+//         }
+//     }
+// }
+
+
+// queue using 2 stacks
 import java.util.*;
-public class queue {
+    public class queue {
+     static class Queue {
+        static Stack<Integer> s1 = new Stack<>();
+        static Stack<Integer> s2 = new Stack<>();
+
+        public static boolean isEmpty() {
+            return s1.isEmpty() && s2.isEmpty();
+        }
+
+        // add 0(n)
+        public static int add(int data) {
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            s1.push(data);
+
+            while(!s2.isEmpty()){
+                s1.push(s2.pop());
+            }
+            return data;
+        }
+        // remove
+        public static int remove() {
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            return s1.pop();
+        }
+        // peek
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            return s1.peek();
+        }
+    }
     public static void main(String args[]) {
-        Queue<Integer> q = new LinkedList<>();  //arrayDeque<>();
+        Queue q = new Queue();
         q.add(1);
         q.add(2);
         q.add(3);
