ec5bcc5b2f996a574a11543602df3a0f826c3952
Yash-G-K <kalbhuteyash@gmail.com>
2025-10-08 00:28:04 +0530

    code changes

--- bst1.java
+++ bst1.java
@@ -84,6 +84,21 @@ public class bst1{
         }
         return root;
     }
+  // pritn the range of the number in BST
+    public static void printInRange(node root,int k1,int k2){
+        if(root==null){
+            return;
+        }
+        if(root.data>=k1 && root.data<=k2){
+            printInRange(root.left,k1,k2);
+            System.out.print(root.data+" ");
+            printInRange(root.right,k1,k2);
+        }else if(root.data<k1){
+            printInRange(root.right,k1,k2);
+        }else{
+            printInRange(root.left,k1,k2);
+        }
+    }
 
     public static void main(String args[]){
         int values[]={8,5,3,1,4,6,10,11,14};
@@ -104,6 +119,10 @@ public class bst1{
 
         root=delete(root,14);
         inorder(root);
+
+        System.out.println();
+        printInRange(root,5,12);
+        System.out.println();
 
     }
 
