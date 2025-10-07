8770c814b3d72850fdede8dab4ce6813c349bd51
Yash-G-K <kalbhuteyash@gmail.com>
2025-10-07 17:30:57 +0530

    code changes

--- btree3.java
+++ btree3.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class btree3 {
     static class btree {
         int data;
@@ -24,6 +25,61 @@ public class btree3 {
         }
     }
 
+    // print path from root to a node
+    public static boolean getPath(btree root, int n, ArrayList<btree> path) {
+        if (root == null) {
+            return false;
+        }
+        path.add(root);
+        if (root.data == n) {
+            return true;
+        }
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+        if (foundLeft || foundRight) {
+            return true;
+        }
+        path.remove(path.size() - 1); // backtracking
+        return false;
+    }
+
+    // ancestors of a node in a binary tree
+    public static btree lca(btree root, int n1, int n2) {
+        ArrayList<btree> path1 = new ArrayList<>();
+        ArrayList<btree> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        // last common ancestor
+        int i = 0;
+        for (; i < path1.size() && i < path2.size(); i++) {
+            if (path1.get(i) != (path2.get(i))) {
+                break;
+            }
+        }
+        // last equal node
+        btree lca = path1.get(i - 1);
+        return lca;
+    }
+
+    // approach 2 for lca
+    public static btree lca2(btree root, int n1, int n2){
+        if(root == null || root.data == n1 || root.data == n2){
+            return root;
+        }
+        btree leftlca = lca2(root.left, n1, n2);
+        btree rightlca = lca2(root.right, n1, n2);
+
+        if(rightlca == null){
+            return leftlca;
+        }
+        if(leftlca == null){
+            return rightlca;
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
         // ✅ Manual tree creation
         btree root = new btree(1);
@@ -37,5 +93,10 @@ public class btree3 {
         int k = 2; // Level to print (root is level 0)
         System.out.println("Nodes at level " + k + ":");
         binarytree.klevel(root, 0, k);
+
+        int n1 = 4, n2 = 6;
+        System.out.println("\nLCA of " + n1 + " and " + n2 + ": " + lca(root, n1, n2).data);
+
+        System.out.println("LCA of " + n1 + " and " + n2 + " using approach 2: " + lca2(root, n1, n2).data);
     }
 }
