1f29a24a0b1f2ebc71362e36bb1d4e4f8b69817b
Yash-G-K <kalbhuteyash@gmail.com>
2025-10-09 00:56:21 +0530

    code changes

--- heaps.java
+++ heaps.java
@@ -1,12 +1,29 @@
-import java.util.Comparator;
 import java.util.PriorityQueue;
 public class heaps{
+    public static class student implements Comparable<student>{
+        String name;    
+        int rank;
+        public student(String name, int rank){
+            this.name = name;
+            this.rank = rank;
+        }
+        @Override
+        public int compareTo(student s2){
+            return this.rank - s2.rank;  //ascending order
+            // return s2.rank - this.rank;  //descending order
+        }
+        @Override
+        public String toString() {
+            return "Student{name='" + name + "', rank=" + rank + "}";
+        }
+    }
+
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq  = new PriorityQueue<>(Comparator.reverseOrder());
-        pq.add(4);  //o(log n)
-        pq.add(1);
-        pq.add(3);
-        pq.add(5);
+        PriorityQueue<student> pq  = new PriorityQueue<>();
+        pq.add(new student("A", 4));  //o(log n)
+        pq.add(new student("B", 6));  //o(log n)
+        pq.add(new student("C", 10));  //o(log n)
+        pq.add(new student("D", 11));  //o(log n)
 
         while(!pq.isEmpty()){
             System.out.println(pq.peek());  //o(1)
