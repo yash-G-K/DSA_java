d9b40d06143f2837a5b0d66ce872965f3a8b55c6
Yash-G-K <kalbhuteyash@gmail.com>
2025-10-09 15:00:26 +0530

    code changes

--- heaps.java
+++ heaps.java
@@ -34,34 +34,138 @@
 
 
 //  Insert opearation - O(log n) in heap
-import java.util.*;
-public class heaps{
-    public static class heap{
-        ArrayList<Integer> arr = new ArrayList<>();
-        public void add(int data){
-            arr.add(data);
+// import java.util.*;
+// public class heaps{
+//     public static class heap{
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         public void add(int data){
+//             arr.add(data);
 
-            int x = arr.size() - 1; //child index
-            int par = (x - 1) / 2; //parent index
+//             int x = arr.size() - 1; //child index
+//             int par = (x - 1) / 2; //parent index
 
-            while(arr.get(x) < arr.get(par)){
-                //swap
-                int temp = arr.get(x);
-                arr.set(x, arr.get(par));
-                arr.set(par, temp);
+//             while(arr.get(x) < arr.get(par)){
+//                 //swap
+//                 int temp = arr.get(x);
+//                 arr.set(x, arr.get(par));
+//                 arr.set(par, temp);
                 
-                x = par;
-                par = (x - 1) / 2;
-            }
+//                 x = par;
+//                 par = (x - 1) / 2;
+//             }
+//         }
+//         public int peek(){
+//             return arr.get(0);
+//         }
+
+//         private void heapify(int i){
+//             int left = 2 * i + 1;
+//             int right = 2 * i + 2;
+//             int minIdx = i;
+
+//             if(left < arr.size() && arr.get(minIdx) > arr.get(left)){
+//                 minIdx = left;
+//             }
+
+//             if(right <arr.size() && arr.get(minIdx) > arr.get(right)){
+//                 minIdx = right;
+//             }
+
+//             if( minIdx != i){
+//                 //swap
+//                 int temp = arr.get(i);
+//                 arr.set(i , arr.get(minIdx));
+//                 arr.set(minIdx, temp);
+
+//                 heapify(minIdx);
+//                 return;
+//             }
+//         }
+
+//         public boolean isEmpty(){
+//             return arr.size() == 0;
+//         }
+
+//     public int remove(){
+//         int data  = arr.get(0);
+//         //step1 - swap first and last
+//         int temp = arr.get(0);
+//         arr.set(0,arr.get(arr.size() - 1));
+//         arr.set(arr.size() - 1, temp);
+
+//         //step2 - remove last
+//         arr.remove(arr.size() - 1);
+//         //step3 - heapify
+//         heapify(0);
+//         return data;
+//     }
+//     }
+
+//     public static void main(String[] args) {
+//         heap h = new heap();
+//         h.add(3);
+//         h.add(4);
+//         h.add(1);
+//         h.add(5);
+//         System.out.println(h.arr);
+
+//         while(!h.isEmpty()){
+//             System.out.println(h.peek());
+//             h.remove();
+//         }
+//     }
+// }
+
+
+// heap sort 
+public class heaps{
+    public static void heapify(int arr[], int i, int n){
+        int left = 2 *i + 1;
+        int right = 2 * i + 2;
+        int maxIdx = i;
+        if(left < arr.length && arr[left] > arr[maxIdx]){
+            maxIdx = left;
+        }
+
+        if(right < arr.length && arr[right] > arr[maxIdx]){
+            maxIdx = right;
+        }
+
+        if(maxIdx != i){
+            //swap 
+            int temp = arr[i];
+            arr[i] = arr[maxIdx];
+            arr[maxIdx] = temp;
+
+            heapify(arr, maxIdx, n);
+            return;
+        }
+    }
+
+    public void heapSort(int arr[]){
+        //step1 - build heap
+        int n = arr.length;
+        for(int i = n/2; i >= 0; i--){
+            heapify(arr, i, n);
+        }
+
+        //step2 - swap and heapify
+        for(int i = n - 1; i > 0; i--){
+            //swap
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, 0, i);
         }
     }
 
     public static void main(String[] args) {
-        heap h = new heap();
-        h.add(3);
-        h.add(4);
-        h.add(1);
-        h.add(5);
-        System.out.println(h.arr);
-    }
+        int arr[] = {1,5,3,4,2};
+        heaps h = new heaps();
+        h.heapSort(arr);
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }  
+    }   
 }
